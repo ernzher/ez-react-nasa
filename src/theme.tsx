9a0273d75b19a8d5d4f3bb289f2867b294891be5
ernzher <leeernzher@gmail.com>
2022-03-04 12:26:26 -0600

    add: button variants and pokemon type colors

--- src/theme.tsx
+++ src/theme.tsx
@@ -3,8 +3,29 @@ import {extendTheme } from "@chakra-ui/react";
 const theme = extendTheme({
     colors: {
         brand: {
-            100: "#30D5C8"
-        }
+            100: "#30D5C8",
+            
+        },
+        type: {
+            grass: "#A8E769",
+            poison: "#B075D1",
+            fire: "#FE7F00",
+            psychic: "#F378B7",
+            bug: "#578744",
+            rock: "#897340",
+            electric: "#E6CC11",
+            ice: "#30D5C8",
+            water: "#1664D8",
+            flying: "#CECACA",
+            ground: "#67470D",
+            fairy: "#FFBEE7",
+            normal: "#636363", 
+            steel: "#929292",
+            fighting: "#D86104",
+            ghost: "#5237FB",
+            dark: "#1C1B1C",
+            dragon: "#6B006B",
+        },
     },
     fonts: {
         heading: 'Lato, sans-serif',
@@ -23,6 +44,16 @@ const theme = extendTheme({
             },
     
         },
+        Button:{
+            variants: {
+                pri_gradient: {
+                    bgGradient: "linear(to-r, , #24adf3, #29deb7)"
+                },
+                snd_gradient: {
+                    bgGradient: "linear(to-r, #6c90ff, #7ef2f6)"
+                }
+            }
+        }
     },
 })
 
